e38967c168122459afaea6540222b9e7e8962773
Julia <avokdorog@mail.ru>
2023-07-17 17:00:39 +0300

    cheange src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Добро пожаловать!");
+
+        int ticketPrice = 13676; // стоимомть билета
+        int oneMile = 20; // сумма начисленния за 1 милю
+
+        int miles; // Объявляете переменные для количества милей
+        if (ticketPrice < 0)  {
+            System.out.println("Стоимость билета не может быть отрицательной");
+        }else{
+            miles = ticketPrice / oneMile; // подсчёт количество милей
+            System.out.println("Ваше количество миль: "  + miles + ". Желаем хорошего полёта!");
+
+        }
+    }
+}
